9aa90f5e22992f303e1c024146302d77b5dbaacf
Saúl Hernández <ivhsaulhdez@gmail.com>
2023-10-22 15:44:58 +0100

    Primer commit

---
+++ src/HolaMundo/holamundo.java
@@ -0,0 +1,9 @@
+package HolaMundo;
+
+public class holamundo {
+	public static void main(String [] args) {
+			System.out.println("1 cambio- Consola");
+			System.out.println("1 cambio- Eclipse");
+	}
+
+}
